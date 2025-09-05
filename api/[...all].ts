3125e1e1f985647cca0b6510422b71f4b643bc99
Matthew Kramer <matthew.kramer@wildflowerschools.org>
2025-09-05 09:35:21 -0500

    moved express server to vercel 7

---
+++ api/[...all].ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { registerRoutes } from '../server/routes';
+import { setupAuth } from '../server/auth';
+
+let appPromise: Promise<express.Express> | null = null;
+
+async function buildApp(): Promise<express.Express> {
+  const app = express();
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+
+  await setupAuth(app);
+  await registerRoutes(app);
+
+  return app;
+}
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  if (!appPromise) appPromise = buildApp();
+  const app = await appPromise;
+  // Delegate to Express app which serves /api/* routes
+  // @ts-ignore - express types are compatible
+  return app(req, res);
+}
+
