799987f998fbe929793b9f36bd32acedf0e46c80
Matthew Kramer <matthew.kramer@wildflowerschools.org>
2025-09-30 14:55:20 -0500

    ready for user testing?

--- client-new/src/modules/charters/views.ts
+++ client-new/src/modules/charters/views.ts
@@ -102,3 +102,4 @@ export const CHARTER_VIEW_SPEC: ViewSpec = view(
   tab('action_steps', 'Action Steps', table('charterActionSteps')),
   tab('grants_loans', 'Grants & Loans', table('charterGrantsLoans')),
   tab('gmail_gCCal', 'Gmail/gCal', table('charterGmails', { title: 'Gmails', width: 'half' }), table('charterCalendarEvents', { title: 'Calendar Events', width: 'half' }))
+);
